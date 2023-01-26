3f55c17950c04fb8244c1389e6485906fe8a27af
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-01-25 21:28:54 -0800

    ui changes

--- script.js
+++ script.js
@@ -13,11 +13,12 @@ const onGenerateSubmit = (e) => {
     if (url === '') {
       alert('Please enter a URL');
     } else {
+      document.getElementById('default-qr').removeAttribute('src');
       showSpinner();
       // Show spinner for 1 sec
       setTimeout(() => {
-        hideSpinner();
         generateQRCode(url, size);
+        hideSpinner();
   
         // Generate the save button after the qr code image src is ready
         setTimeout(() => {
@@ -31,39 +32,39 @@ const onGenerateSubmit = (e) => {
   };
 
 const generateQRCode = (url, size) => {
-    const qrcode = new QRCode('qrcode', {
-        text: url,
-        width: size,
-        height: size
-    });
+  const qrcode = new QRCode('qrcode', {
+    text: url,
+    width: size,
+    height: size
+  });
 }
 
 const showSpinner = () => {
-   document.getElementById('spinner').style.display = 'block'; 
+  document.getElementById('spinner').style.display = 'block'; 
 }
 
 const hideSpinner = () => {
-    document.getElementById('spinner').style.display = 'none'; 
+  document.getElementById('spinner').style.display = 'none'; 
 }
 
 const clearUI = () => {
-    qr.innerHTML = '';
-    const saveLink = document.getElementById('save-link');
-    if(saveLink){
-        saveLink.remove();
-    }
+  qr.innerHTML = '';
+  const saveLink = document.getElementById('save-link');
+  if(saveLink){
+    saveLink.remove();
+  }
 }
 
 const createSaveBtn = (saveUrl) => {
-    const link = document.createElement('a');
-    link.id = 'save-link';
-    link.classList =
-      'bg-sky-500 hover:bg-sky-700 text-white text-center font-bold py-2 rounded w-1/3 m-auto my-5';
-    link.href = saveUrl;
-    link.download = 'qrcode';
-    link.innerHTML = 'Save Image';
-    document.getElementById('generated').appendChild(link);
-  };
+  const link = document.createElement('a');
+  link.id = 'save-link';
+  link.classList =
+    'bg-sky-500 hover:bg-sky-700 text-white text-center font-bold py-2 rounded w-full m-auto my-5';
+  link.href = saveUrl;
+  link.download = 'qrcode';
+  link.innerHTML = 'Download PNG';
+  document.getElementById('generated').appendChild(link);
+};
 
 hideSpinner();
 
